c8edcd8cd02771786e3daba9efdb2f2e95cf3891
ctoothaker <charles.toothaker@maine.edu>
2018-12-09 19:29:33 -0500

    Finished guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,34 @@
+function  CountDirFiles {
+	numFiles=0
+	let numFiles=$(ls -l | wc -l)
+}
+
+numFiles=0
+guess=-1
+regex="^[0-9]+$"
+
+CountDirFiles
+
+echo "How many files do you believe are in this directory?"
+
+while [[ $guess -ne $numFiles ]]
+do
+	read guess
+
+	if ! [[ $guess  =~ $regex ]]
+	then
+		echo "Please enter a valid number as your guess."
+		continue
+	fi
+
+	if [[ $guess -eq $numFiles ]]
+	then
+		echo "That is correct!"
+	elif [[ $guess -gt $numFiles ]]
+	then
+		echo "You have guessed too high, please try again."
+	elif [[ $guess -lt $numFiles ]]
+	then
+		echo "You have guessed too low, please try again."
+	fi
+done
